11b595388575b6d1c0e30ce2d9258d0ff6f8ae41
yoyogias <zome2013@gmail.com>
2020-03-19 15:45:57 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# friendlymarket-codelab-is1tax
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/friendlymarket-codelab-is1tax)
---
+++ src/firebase-config.js
@@ -0,0 +1,15 @@
+/* 
+  Your Firebase config from the Firebase console 
+  https://firebase.google.com/docs/web/setup#config-object
+*/
+const firebaseConfig = {
+  apiKey: 'TODO',
+  authDomain: 'TODO',
+  databaseURL: 'TODO',
+  projectId: 'TODO',
+  storageBucket: 'TODO',
+  messagingSenderId: 'TODO',
+  appId: 'TODO'
+};
+
+export default firebaseConfig;
